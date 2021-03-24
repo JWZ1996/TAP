161827564623b2777f1d900d9a71d50f754fd1d8
JWZ1996 <jwz@DESKTOP-QDJVS1O.localdomain>
2021-03-24 13:52:17 +0100

    poprawka linearyzacji

--- linear.m
+++ linear.m
@@ -2,19 +2,14 @@ syms Ca g m K mol kmol cal ro cp k0 E_R h a b ro cp k0 E_R h a b V Fin CAin Fc T
 
 % TODO - dopytac sie piotera czy napewno ok
 
-f1 = V*k0*exp(-E_R/T)*Ca;
+f1 = -V*k0*exp(-E_R/T)*Ca;
 f2 = V*h*k0*exp(-E_R/T)*Ca - (a*Fc^(b+1)/(Fc+(a*Fc^b/(2*ro*cp))))*(T-Tin);
 
-ptDefault = [0.16 405];
-lin1 = getLinearized(f1,ptDefault);
-lin2 = getLinearized(f2,ptDefault);
+ptDefaultF1 = [1 0.16 405];
+ptDefaultF2 = [1 0.16 405 310 15];
 
-ptCalculated = []
-lin1_2 = getLinearized(f1,ptCalculated);
-lin2_2 = getLinearized(f2,ptCalculated);
+lin1 = taylor(f1,[V Ca T],        ptDefaultF1,'Order',2);
+lin2 = taylor(f2,[V Ca T Tcin Fc],ptDefaultF2,'Order',2);
 
-
-function lin = getLinearized(f,pkt)
-syms Ca g m K mol kmol cal ro cp k0 E_R h a b ro cp k0 E_R h a b V Fin CAin Fc Tin Tcin T;
-lin = taylor(f,[Ca T],pkt,'Order',2);
-end
+lin1Simple = simplify(lin1);
+lin2Simple = simplify(lin2);
