ae5e62bb8c0c0d2f8b200796e3e2b70cb89ca4b7
JWZ1996 <jwz@DESKTOP-QDJVS1O.localdomain>
2021-03-29 00:28:03 +0200

    Poprawka linearyzacji

--- consts.m
+++ consts.m
@@ -1,6 +1,6 @@
 
 % ==== Plik zawierajacy globalne stale =====
-global g m K mol kmol cal min ro cp k0 E_R h a b ro cp k0 E_R h a b V Fin CAin Fc Tin Tcin Ca T V0 Fin0 CAin0 Fc0 Tin0 Tcin0 Ca0 T0;
+global g m K mol kmol cal min ro cp k0 E_R h a b ro cp k0 E_R h a b V Fin CAin Fc Tin Tcin Ca T F V0 Fin0 CAin0 Fc0 Tin0 Tcin0 Ca0 T0;
 % ==== Jednostki ====
 % TODO ja lubie cukier syntaktyczny, ale jbc sie wyrzuci
 
@@ -29,6 +29,7 @@ V = 1 * m^3;
 Fin = 1 * m^3/min;
 CAin = 2 * kmol/m^3;
 Fc = 15 * m^3/min;
+F = Fc;
 Tin = 343 * K;
 Tcin = 310 * K;
 Ca = 0.16 * kmol/m^3;
