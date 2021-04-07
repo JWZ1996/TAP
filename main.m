f228a868ed01a93a349b060caa8ac2c8f188ee85
Piotr Tomikowski <01112229@pw.edu.pl>
2021-04-07 11:43:21 +0200

    dodanie wykresu T(Ca)

--- main.m
+++ main.m
@@ -9,38 +9,68 @@ step = 0.01;
 Ca = 0.042953 * kmol/m^3;
 T =  431.176 * K;
 
-CAin_vect = [2.4, 2.2, 1.8, 1.6, 1.4 2].*kmol;
-Fc_vect = [18,17,16,14,13,12, 15];
+CAin_vect = [2.6, 2.4, 2.2, 1.8, 1.6, 1.4, 2].*kmol;
+Fc_vect = [12,13,14,16,17,18, 15];
 
 %% ============================
+t=0:step:5;
+y(:,1) = [Ca T];
+    for i=1:(length(t)-1)
+        k11=dCa(Ca,T);
+        k12=dT(Ca,T);
+
+        k21=dCa(Ca+0.5*step,T+0.5*step*k11);
+        k22=dT(Ca+0.5*step,T+0.5*step*k12);
+
+        k31=dCa(Ca+0.5*step,T+0.5*step*k21);
+        k32=dT(Ca+0.5*step,T+0.5*step*k22);
+
+        k41=dCa(Ca+step,T+step*k31);
+        k42=dT(Ca+step,T+step*k32);
+
+        Ca=Ca+(step/6)*(k11+k41+2*(k21+k31));
+        T=T+(step/6)*(k12+k42+2*(k22+k32));
+        y(:,i+1)=[Ca T];
+    end
+plot(y(1,:),y(2,:), '*')
+%% ============================
 %==========    b)   =============
+
 figure(1)
-PlotModel('CAin', 'Ca', CAin_vect, @dCa, @dT, step, 'Stê¿enie Ca w funkcji czasu - skok CAin', ':');
-PlotModel('CAin', 'Ca', CAin_vect, @dCaLin, @dTLin, step, 'Stê¿enie Ca w funkcji czasu - skok CAin', '--');
+PlotModel('CAin', 'Ca', CAin_vect, @dCa, @dT, step, 'St�enie Ca w funkcji czasu - skok CAin', ':');
+PlotModel('CAin', 'Ca', CAin_vect, @dCaLin, @dTLin, step, 'St�enie Ca w funkcji czasu - skok CAin', '--');
+plotLegend('Ca', CAin_vect)
 hold off
 
-CAin = 2;
+CAin = 2; Fc = 15;
 
 figure(2)
-PlotModel('Fc', 'Ca', Fc_vect, @dCa, @dT, step, 'Stê¿enie Ca w funkcji czasu - skok Fc', ':');
-PlotModel('Fc', 'Ca', Fc_vect, @dCaLin, @dTLin, step, 'Stê¿enie Ca w funkcji czasu - skok Fc', '--');
+PlotModel('Fc', 'Ca', Fc_vect, @dCa, @dT, step, 'St�enie Ca w funkcji czasu - skok Fc', ':');
+PlotModel('Fc', 'Ca', Fc_vect, @dCaLin, @dTLin, step, 'St�enie Ca w funkcji czasu - skok Fc', '--');
+plotLegend('Fc', Fc_vect)
 hold off
+
+CAin = 2; Fc = 15;
 
 figure(3)
 PlotModel('CAin', 'T', CAin_vect, @dCa, @dT, step, 'Temperatura T w funkcji czasu - skok CAin', ':');
 PlotModel('CAin', 'T', CAin_vect, @dCaLin, @dTLin, step, 'Temperatura T w funkcji czasu - skok CAin', '--');
+plotLegend('Ca', CAin_vect)
 hold off
+
+CAin = 2; Fc = 15;
 
 figure(4)
 PlotModel('Fc', 'T', Fc_vect, @dCa, @dT, step, 'Temperatura T w funkcji czasu - skok Fc', ':');
 PlotModel('Fc', 'T', Fc_vect, @dCaLin, @dTLin, step, 'Temperatura T w funkcji czasu - skok Fc', '--');
+plotLegend('Fc', Fc_vect)
 hold off
 
-Fc = 15;
+CAin = 2; Fc = 15;
 %% =============================
 %========Model nieliniowy=========
 figure(1)
-PlotModel('CAin', 'Ca', CAin_vect, @dCa, @dT, step, 'Stê¿enie Ca w funkcji czasu - skok CAin', '-');
+PlotModel('CAin', 'Ca', CAin_vect, @dCa, @dT, step, 'St�enie Ca w funkcji czasu - skok CAin', '-');
 hold off
 
 figure(2)
@@ -50,7 +80,7 @@ hold off
 CAin = 2;
 
 figure(3)
-PlotModel('Fc', 'Ca', Fc_vect, @dCa, @dT, step, 'Stê¿enie Ca w funkcji czasu - skok Fc', '-');
+PlotModel('Fc', 'Ca', Fc_vect, @dCa, @dT, step, 'St�enie Ca w funkcji czasu - skok Fc', '-');
 hold off
 
 figure(4)
@@ -61,7 +91,7 @@ Fc = 15;
 %% =============================
 %======Model zlinearyzowany=======
 figure(5)
-PlotModel('CAin', 'Ca', CAin_vect, @dCaLin, @dTLin, step, 'Stê¿enie Ca w funkcji czasu - skok CAin', '-');
+PlotModel('CAin', 'Ca', CAin_vect, @dCaLin, @dTLin, step, 'St�enie Ca w funkcji czasu - skok CAin', '-');
 hold off
 
 figure(6)
@@ -71,7 +101,7 @@ hold off
 CAin = 2;
 
 figure(7)
-PlotModel('Fc', 'Ca', Fc_vect, @dCaLin, @dTLin, step, 'Stê¿enie Ca w funkcji czasu - skok Fc', '-');
+PlotModel('Fc', 'Ca', Fc_vect, @dCaLin, @dTLin, step, 'St�enie Ca w funkcji czasu - skok Fc', '-');
 hold off
 
 figure(8)
@@ -86,7 +116,7 @@ Fc = 15;
 Ts = 10; %  -> sampling time = 0.1min = 6s
 
 figure(9)
-PlotModelDiscrete('CAin', 'Ca', Ts, CAin_vect, @dCaLin, @dTLin, step, 'Stê¿enie Ca w funkcji czasu - skok CAin', '-');
+PlotModelDiscrete('CAin', 'Ca', Ts, CAin_vect, @dCaLin, @dTLin, step, 'St�enie Ca w funkcji czasu - skok CAin', '-');
 hold off
 
 figure(10)
@@ -95,7 +125,7 @@ hold off
 
 CAin = 2;
 figure(11)
-PlotModelDiscrete('Fc', 'Ca', Ts, Fc_vect, @dCaLin, @dTLin, step, 'Stê¿enie Ca w funkcji czasu - skok Fc', '-');
+PlotModelDiscrete('Fc', 'Ca', Ts, Fc_vect, @dCaLin, @dTLin, step, 'St�enie Ca w funkcji czasu - skok Fc', '-');
 hold off
 
 figure(12)
@@ -186,6 +216,13 @@ end
  
 function PlotModel(InVar, OutVar, vect, dCaIn, dTIn, step, titleText, plotStyle)
     global Ca T CAin Fc;
+    colour_vect = [ 0.9294,    0.6941,    0.1255;
+                    0,    	   0.4471,    0.7412;
+                    0.4941,    0.1843,    0.5569;
+                    0.1490,    0.1490,    0.1490;
+                    0.6353,    0.0784,    0.1843;
+                    1.0000,    0.4118,    0.1608;
+                    0.0549,    0.6196,    0.9020];
     for iter = 1:1:length(vect)
         if (strcmp(InVar,'CAin') == 1)
             CAin = vect(iter);
@@ -196,19 +233,31 @@ function PlotModel(InVar, OutVar, vect, dCaIn, dTIn, step, titleText, plotStyle)
             y_plot = 1;
         elseif (strcmp(OutVar,'T') == 1)
             y_plot = 2;   
-        end
-           
-        [y, t] = rk4(dCaIn, dTIn, Ca, T, step); %cze�� Zan Jembowicz tutaj mam pytanie we� no sp�jrz kurcz� jak tu wstawi� do @dCa wszystko na co mam ochot�
-        txt = [InVar ' = ',num2str(vect(iter))];
-        plot(t,y(y_plot,:),plotStyle,'DisplayName',txt);
+        end    
+        [y, t] = rk4(dCaIn, dTIn, Ca, T, step);
+        %txt = [InVar ' = ',num2str(vect(iter))];
+        plot(t,y(y_plot,:),plotStyle,'Color', colour_vect(iter,:),'LineWidth',1.5); %'DisplayName',txt,
         title(titleText)
+        xlabel('t [min]')
+        if (strcmp(OutVar,'Ca') == 1)
+            ylabel('Ca [kmol/m^3]');
+        elseif (strcmp(OutVar,'T') == 1)
+            ylabel('T [K]');  
+        end
         hold on
     end
-    legend show
-end %o tu pytanie b�dzie Jan
+    %legend show
+end
 
-function PlotModelDiscrete(InVar, OutVar, Ts, vect, dCa, dT, step, titleText, plotStyle)
+function PlotModelDiscrete(InVar, OutVar, Ts, vect, dCaIn, dTIn, step, titleText, plotStyle)
     global Ca T CAin Fc;
+    colour_vect = [ 0.9294,    0.6941,    0.1255;
+                    0,    	   0.4471,    0.7412;
+                    0.4941,    0.1843,    0.5569;
+                    0.1490,    0.1490,    0.1490;
+                    0.6353,    0.0784,    0.1843;
+                    1.0000,    0.4118,    0.1608;
+                    0.0549,    0.6196,    0.9020];
     for iter = 1:1:length(vect)
         if (strcmp(InVar,'CAin') == 1)
             CAin = vect(iter);
@@ -217,16 +266,47 @@ function PlotModelDiscrete(InVar, OutVar, Ts, vect, dCa, dT, step, titleText, pl
         end
         if (strcmp(OutVar,'Ca') == 1)
             y_plot = 1;
+            ylabel('Ca [kmol/m^3]');
         elseif (strcmp(OutVar,'T') == 1)
-            y_plot = 2;   
+            y_plot = 2;
+            ylabel('T [K]'); 
         end
-           
-        [y, t] = rk4Discrete(@dCa, @dT, Ca, T, step, Ts);
-        txt = [InVar ' = ',num2str(vect(iter))];
-        plot(t,y(y_plot,:), plotStyle,'DisplayName',txt);
+   
+        [y, t] = rk4Discrete(dCaIn, dTIn, Ca, T, step, Ts);
+        %txt = [InVar ' = ',num2str(vect(iter))];
+        plot(t,y(y_plot,:),plotStyle,'Color',colour_vect(iter,:),'LineWidth',1.5); %'DisplayName',txt,
         title(titleText)
+        if (strcmp(OutVar,'CA') == 1)
+            ylabel('Ca [kmol/m^3]');
+        elseif (strcmp(OutVar,'T') == 1)
+            ylabel('T [K]');  
+        end
         hold on
     end
-    legend show
+    %legend show
+end
+
+function plotLegend(InVar, vect)
+colour_vect = [ 0.9294,    0.6941,    0.1255;
+                0,    	   0.4471,    0.7412;
+                0.4941,    0.1843,    0.5569;
+                0.1490,    0.1490,    0.1490;
+                0.6353,    0.0784,    0.1843;
+                1.0000,    0.4118,    0.1608;
+                0.0549,    0.6196,    0.9020];
+h = zeros(size(colour_vect, 1));
+h(1) = plot(NaN,NaN,'-','Color',colour_vect(1,:));
+h(2) = plot(NaN,NaN,'-','Color',colour_vect(2,:));
+h(3) = plot(NaN,NaN,'-','Color',colour_vect(3,:));
+h(4) = plot(NaN,NaN,'-','Color',colour_vect(4,:));
+h(5) = plot(NaN,NaN,'-','Color',colour_vect(5,:));
+h(6) = plot(NaN,NaN,'-','Color',colour_vect(6,:));
+h(7) = plot(NaN,NaN,'-','Color',colour_vect(7,:));
+equals = ' = ';
+legend(   char(strcat(InVar,equals,{' '},num2str(vect(1)))), char(strcat(InVar,equals,{' '},num2str(vect(2)))), ...
+          char(strcat(InVar,equals,{' '},num2str(vect(3)))), char(strcat(InVar,equals,{' '},num2str(vect(4)))), ...
+          char(strcat(InVar,equals,{' '},num2str(vect(5)))), char(strcat(InVar,equals,{' '},num2str(vect(6)))), ...
+          char(strcat(InVar,equals,{' '},num2str(vect(7)))) ...
+          );
 end
 
