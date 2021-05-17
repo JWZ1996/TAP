e1f68dfb6093d8ee92a7a1cf62a244a4fcaa613f
jmatyas <jakubk.m@gmail.com>
2021-05-17 02:05:05 +0200

    Add proper support for numeric variant; Add comparison to plot_splitter

--- MPCS_numeric.m
+++ MPCS_numeric.m
@@ -1,16 +1,14 @@
-clear all;
-% close all;
+function [] = MPCS_numeric(Nu, N, Ts, Tsim, lambda1, lambda2, psi1, psi2)
 
 global Ca0 T0 Tin CAin0 Fc0;
-%%
-N=10; %horyzont predykcji
-Nu=4 ; %horyzont sterowania
+% N=20; %horyzont predykcji
+% Nu=15 ; %horyzont sterowania
 ny=2; %ilosc wyjsc
 nu=2; %ilosc sterowań
-
-Ts=0.01; %czas probkowania
-Tsim=10; %czas symulacji
-time=0:Ts:Tsim;
+% 
+% Ts=0.01; %czas probkowania
+% Tsim=30; %czas symulacji
+time=0:Ts:Tsim-Ts;
 
 [A, B, C, D] = import_ABCD();
 % dyskretyzacja parametrow 
@@ -22,15 +20,17 @@ C=disc.C;
 D=disc.D;
 
 [M,CtAt,CtV]=MPCSmatrices(A,B,C,N,Nu); % wyznaczenie macierzy MPCS
+psi = [psi1 psi2];
+psi=diag(repmat(psi,1,N));
 
-psi=eye(N*ny); %macierz psi, czyli wartosciowanie wyjsc
-
-lambda1=0.01;
-lambda2=0.01;
+% psi=eye(N*ny); %macierz psi, czyli wartosciowanie wyjsc
+% 
+% lambda1=0.01;
+% lambda2=0.01;
 lambda=[lambda1 lambda2];
 lambda=diag(repmat(lambda,1,Nu));
 
-K=(M'*psi*M+lambda)\M'*psi; %macierz K
+K=(M'*psi*M+lambda)^(-1)*M'*psi; %macierz K
 K1=K(1:nu,:); %nu pierwszych wierszy K
 
 options_quad = optimoptions('quadprog','Display','off');
@@ -38,76 +38,113 @@ options_quad = optimoptions('quadprog','Display','off');
 H = 2*(M'*psi*M+lambda);
 J= tril(repmat(eye(nu),Nu));
 Aopt = [-J;J;-M;M];
-%%
-T_scale_factor = 100; % wspolczynnik skalujacy temperature dla lepszych wlasnosci numerycznych 
-v=[0;0]; %wektor zaklocen stanu
 
+v=[0;0]; %wektor zaklocen stanu
 % macierze sterowan (nu x Nu) i przyrostow sterowan (nu x Nu)
 U=zeros(nu,Nu);  
-du = zeros(nu,Nu);
-x = du;
+% du = zeros(nu,Nu);
 
 % wektor trajektorii swobodnej wyjsc
 Y0=zeros(ny*N, 1);
 
 % wektor przewidywanych wartosci wyjsc regulowanych
-Y_pred=zeros(ny,1);
+y=zeros(ny,1);
 
-Y_zad = repmat([Ca0; T0/T_scale_factor], N,1);
-states = zeros(ny, N);
+Y_zad = zeros(N*ny);
+x = zeros(ny, N);
 
 % ograniczenia na wejscia sterowane
 % U = [Cain; Fc]
-Umin= repmat([0; 0],Nu,1);
-Umax= repmat([40;100], Nu,1);
+Umin= repmat([1-CAin0; 0-Fc0],Nu,1);
+Umax= repmat([3-CAin0;30-Fc0], Nu,1);
 
 % ograniczenia na wyjscia regulowane
 % Y = [Ca; T]
-Ymin=repmat([0; 0], N, 1);
-Ymax=repmat([50; 450/T_scale_factor], N, 1);
+Ymin=repmat([-0.01; -5], N, 1);
+Ymax=repmat([0.01; 5], N, 1);
 
-%%
-for k=2:Tsim/Ts
-        
-    v(:,k) = states(:,k) - A*states(:,k-1) - B*U(:,k-1); %wyznaczanie zaklocen stanu
-    Y0(:,k) = CtAt*states(:,k)+CtV*B*U(:,k-1) + CtV*v(:,k); % trajktoria swobodna na podstawie wektora stanu
+for iter=2:Tsim/Ts
+    % zmiana wartości zadanych
+    if(iter<0.1*length(time))
+        yzad=[0; 0]; 
+        Y_zad(1:2:end,iter)=yzad(1);
+        Y_zad(2:2:end,iter)=yzad(2);
+    elseif(iter<0.2*length(time)) 
+        yzad=[0; -2]; 
+        Y_zad(1:2:end,iter)=yzad(1);
+        Y_zad(2:2:end,iter)=yzad(2);    
+    elseif(iter<0.4*length(time)) 
+        yzad=[0.01; -2]; 
+        Y_zad(1:2:end,iter)=yzad(1);
+        Y_zad(2:2:end,iter)=yzad(2);
+    elseif(iter<0.6*length(time)) 
+        yzad=[0.01; 2]; 
+        Y_zad(1:2:end,iter)=yzad(1);
+        Y_zad(2:2:end,iter)=yzad(2);
+	elseif(iter<0.8*length(time)) 
+        yzad=[-0.01; -5]; 
+        Y_zad(1:2:end,iter)=yzad(1);
+        Y_zad(2:2:end,iter)=yzad(2);
+    else
+        yzad=[0; 0]; 
+        Y_zad(1:2:end,iter)=yzad(1);
+        Y_zad(2:2:end,iter)=yzad(2);
+    end    
+    v(:,iter) = x(:,iter) - A*x(:,iter-1) - B*U(:,iter-1); %wyznaczanie zaklocen stanu
+    Y0(:,iter) = CtAt*x(:,iter)+CtV*B*U(:,iter-1) + CtV*v(:,iter); % trajktoria swobodna na podstawie wektora stanu
     
-    f = -2*M'*psi*(Y_zad-Y0(:,k));
+    f = -2*M'*psi*(Y_zad(:,iter)-Y0(:,iter));
     
     % wyznaczanie wektorow ograniczen online
-    U_prev=repmat(U(:,k-1), Nu,1);
-    b = [-Umin+U_prev;Umax-U_prev;-Ymin+Y0(:,k);Ymax-Y0(:,k)];
+    U_prev=repmat(U(:,iter-1),Nu,1);
+    b = [-Umin+U_prev;Umax-U_prev;-Ymin+Y0(:,iter);Ymax-Y0(:,iter)];
     % ograniczenia na wartosci skokow sterowan
-    lb = repmat([-10;-10], Nu,1);
-    ub = repmat([10;10], Nu,1);
+    lb = repmat([-2;-15], Nu,1);
+    ub = repmat([3;10], Nu,1);
     
     [du, fval, exitflag, output] = quadprog(H,f,Aopt,b,[],[],lb,ub,[],options_quad);
-    
+
     if isempty(du)
         break;
     else
-        U(:,k)=U(:,k-1) + du(1:nu);
-        Y_pred(:,k)=C*states(:,k)+D*U(:,k);
-        states(:,k+1)=A*states(:,k)+B*U(:,k) + v(:,k);
+        U(:,iter)=U(:,iter-1) + du(1:nu);
+        y(:,iter)=C*x(:,iter)+D*U(:,iter);
+        x(:,iter+1)=A*x(:,iter)+B*U(:,iter) + v(:,iter);
     end
 
 end
-%%
 
-min_len = min([size(Y_pred,2) size(Y0,2)]);
-Y_zad = repmat(Y_zad(1:2),1, min_len);
+subplot(2,2,1);
+grid on;
+hold on;
+plot(time,y(1,:)+Ca0);
+title('Wyjście 1 - stężenie substancji A')
+xlabel('Czas symulacji [min]')
+ylabel('Ca [kmol/m3]') 
+
+subplot(2,2,2);
+grid on;
+hold on;
+plot(time,y(2,:)+T0);
+title('Wyjście 2 - temperatura')
+xlabel('Czas symulacji [min]')
+ylabel('T [K]') 
 
 
-figure;
-subplot(1,2,1);
-plot(time(1:min_len), Y0(1,:), time(1:min_len), Y_zad(1,:), time(1:min_len), Y_pred(1,:));
-legend('Ca swobodne', 'Wartosc zadana', 'Ca przewidziane');
-xlabel('Czas');
-ylabel('Stezenie Ca [kmol/m3]')
-subplot(1,2,2);
-plot(time(1:min_len), Y0(2,:)*T_scale_factor, time(1:min_len), Y_zad(2,:)*T_scale_factor, time(1:min_len), Y_pred(2,:)*T_scale_factor);
-legend("T swobodne", "Wartosc zadana", 'T przewidziane');
-xlabel('Czas');
-ylabel('Temperatura T [K]')
+subplot(2,2,3);
+grid on;
+hold on;
+plot(time,U(1,:)+CAin0);
+title('Sterowanie 1 - wejściowe stężenie substancji A')
+xlabel('Czas symulacji [min]')
+ylabel('CAin [kmol/m3]') 
 
+
+subplot(2,2,4);
+grid on;
+hold on;
+plot(time,U(2,:)+Fc0);
+title('Sterowanie 2 - strumień objętości substancji chłodzącej')
+xlabel('Czas symulacji [min]')
+ylabel('Fc [m3/min]')
 
--- MPCS_plot_spitter.m
+++ MPCS_plot_spitter.m
@@ -1,49 +1,66 @@
-clear all;
-close all;
+% clear all;
+% close all;
 
 N = 50; Nu = 50; Ts = 0.01; Tsim = 30; l1 = 1; l2 = 1; p1 = 1; p2 = 1;
 
 figure('units','normalized','outerposition',[0 0 1 1]) %1
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
 
 figure('units','normalized','outerposition',[0 0 1 1]) %2
 N = 30; Nu = 30;
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+
 
 figure('units','normalized','outerposition',[0 0 1 1]) %3
 l1 = 0.1; l2 = 0.1;
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
 
 figure('units','normalized','outerposition',[0 0 1 1]) %4
 l1 = 0.01; l2 = 0.01;
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
 
 figure('units','normalized','outerposition',[0 0 1 1]) %5
 l1 = 0.005; l2 = 0.005;
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
 
 figure('units','normalized','outerposition',[0 0 1 1]) %6
 N = 20; Nu = 20;
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
 
 figure('units','normalized','outerposition',[0 0 1 1]) %7
 N = 15; Nu = 10;
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+
 
 figure('units','normalized','outerposition',[0 0 1 1]) %8
-N = 15; Nu = 10; p1 = 30; p2 = 30;
+N = 10; Nu = 3; p1 = 30; p2 = 30; l1 = 0.005; l2 = 0.005;
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
 
-figure('units','normalized','outerposition',[0 0 1 1]) %10
-N = 10; Nu = 3;
-MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
-
-figure('units','normalized','outerposition',[0 0 1 1]) %10
+figure('units','normalized','outerposition',[0 0 1 1]) %9
 N = 10; Nu = 1;
 MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+
+figure('units','normalized','outerposition',[0 0 1 1]) %10
+N = 15; Nu = 3; l1 = 0.001; l2 = 0.001; p1 = 30; p2 = 30;
+MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+
+figure('units','normalized','outerposition',[0 0 1 1]) %11
+N = 30; Nu = 30; l1 = 0.005; l2 = 0.005; p1 = 0.1; p2 = 0,1;
+MPCS_an_func(Nu, N, Ts, Tsim, l1, l2, p1, p2)
+MPCS_numeric(Nu, N, Ts, Tsim, l1, l2, p1, p2)
 
 h=findobj('type','figure'); % find the handles of the opened figures
-folder='D:\Studia\EiTI\TAP\PROJEKT\obrazki_auto';  % Desination folder
+folder='D:\Studia\21L\TAP\TAP\obrazki_auto';  % Desination folder
 for k=1:numel(h)
   filename=sprintf('fig%d.jpg',numel(h)-k+1);
   file=fullfile(folder,filename);
